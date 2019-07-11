9f8ec9ba37ea341a1e825647d2dfd90dd3eb170d
ReinWD <ReinWDD@gmail.com>
2019-07-12 02:20:01 +0800

    add ability triggers

--- src/main/java/cat/nyaa/infiniteinfernal/abilitiy/AbilityAttack.java
+++ src/main/java/cat/nyaa/infiniteinfernal/abilitiy/AbilityAttack.java
@@ -1,7 +1,8 @@
 package cat.nyaa.infiniteinfernal.abilitiy;
 
 import cat.nyaa.infiniteinfernal.mob.IMob;
+import org.bukkit.entity.LivingEntity;
 
 public interface AbilityAttack {
-    void onAttack(IMob mob);
+    void onAttack(IMob mob, LivingEntity target);
 }
---
+++ src/main/java/cat/nyaa/infiniteinfernal/abilitiy/AbilitySpawn.java
@@ -0,0 +1,7 @@
+package cat.nyaa.infiniteinfernal.abilitiy;
+
+import cat.nyaa.infiniteinfernal.mob.IMob;
+
+public interface AbilitySpawn {
+    void onSpawn(IMob iMob);
+}
