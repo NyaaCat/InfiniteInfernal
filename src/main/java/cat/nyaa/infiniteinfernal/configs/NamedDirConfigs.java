1208d1992f37ea6ab6f5b3e0a106a40309c19323
ReinWD <ReinWDD@gmail.com>
2020-04-10 17:28:24 +0800

    update NamedDirConfigs

--- src/main/java/cat/nyaa/infiniteinfernal/configs/NamedDirConfigs.java
+++ src/main/java/cat/nyaa/infiniteinfernal/configs/NamedDirConfigs.java
@@ -1,6 +1,7 @@
 package cat.nyaa.infiniteinfernal.configs;
 
 import cat.nyaa.nyaacore.configuration.ISerializable;
+import org.bukkit.Bukkit;
 import org.bukkit.configuration.InvalidConfigurationException;
 import org.bukkit.configuration.file.YamlConfiguration;
 
@@ -12,6 +13,7 @@ import java.util.Collection;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Set;
+import java.util.logging.Level;
 
 public class NamedDirConfigs<T extends NamedFileConfig> implements ISerializable {
     private Map<String, T> configs = new LinkedHashMap<>();
@@ -38,7 +40,21 @@ public class NamedDirConfigs<T extends NamedFileConfig> implements ISerializable
     }
 
     public T remove(String id) {
-        return configs.remove(id);
+        T remove = configs.remove(id);
+        removeFromDir(remove);
+        return remove;
+    }
+
+    private void removeFromDir(T removed) {
+        if (removed == null)return;
+        if (storageDir.exists()) {
+            File cfgFile = removed.getFile();
+            if (cfgFile.exists()){
+                if (!cfgFile.delete()) {
+                    Bukkit.getLogger().log(Level.SEVERE, "can't remove file "+cfgFile.getPath());
+                }
+            }
+        }
     }
 
     public Set<String> keys() {
@@ -61,10 +77,11 @@ public class NamedDirConfigs<T extends NamedFileConfig> implements ISerializable
                         T t = targetClass.getConstructor(String.class).newInstance(name);
 //                            if (t.getFileName().equals(file.getName())) {
 //                                t.load();
-                        configs.put(name, t);
                         YamlConfiguration config = new YamlConfiguration();
                         config.load(file);
                         t.deserialize(config);
+                        t.setFile(file);
+                        configs.put(name, t);
 //                            }
                     } catch (InstantiationException | IllegalAccessException | NoSuchMethodException | NumberFormatException | InvocationTargetException e) {
                         e.printStackTrace();
--- src/main/java/cat/nyaa/infiniteinfernal/configs/NamedFileConfig.java
+++ src/main/java/cat/nyaa/infiniteinfernal/configs/NamedFileConfig.java
@@ -4,9 +4,12 @@ import cat.nyaa.infiniteinfernal.InfPlugin;
 import cat.nyaa.nyaacore.configuration.FileConfigure;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.io.File;
+
 public abstract class NamedFileConfig extends FileConfigure {
     public String fileName = "";
     protected String prefix = "";
+    private File file;
 
     public NamedFileConfig(String name){
         this.fileName = name;
@@ -25,6 +28,21 @@ public abstract class NamedFileConfig extends FileConfigure {
         return getFileDirName().concat("/")
                 .concat(getName())
                 .concat(".yml");
+    }
+
+    public File getFile(){
+        if (file == null){
+            synchronized (this){
+                if (file == null){
+                    file = new File(getPlugin().getDataFolder(), getFileName());
+                }
+            }
+        }
+        return file;
+    }
+
+    public void setFile(File file) {
+        this.file = file;
     }
 
     protected abstract String getFileDirName();
